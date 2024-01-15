62c905047e301a306fb2a38fd0ebf5e7d007d4c8
luciferro01 <lucifer1111ronaldo@gmail.com>
2024-01-15 12:01:39 +0530

    Adding Course Detail Screen Components and graphql query to fetch the data

--- App/Screens/CourseDetailScreen.js
+++ App/Screens/CourseDetailScreen.js
@@ -1,12 +1,55 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { TouchableOpacity, ScrollView, ToastAndroid } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import DetailSection from "../Components/CourseDetailScreen/DetailSection";
+import ChapterSection from "../Components/CourseDetailScreen/ChapterSection";
+import { enrollCourse, getUserEnrolledCourse } from "../services";
 
-const CourseDetailScreen = () => {
+import { useUser } from "@clerk/clerk-expo";
+export default function CourseDetailScreen() {
+  const navigate = useNavigation();
+  const params = useRoute().params;
+
+  const [userEnrolledCourse, setUserEnrolledCourse] = useState([]);
+  const { user } = useUser();
+  useEffect(() => {
+    if (user && params.course) {
+      GetUserEnrolledCourse();
+    }
+  }, [params.course, user]);
+
+  const UserEnrollCourse = () => {
+    enrollCourse(params.course.id, user.primaryEmailAddress.emailAddress).then(
+      (resp) => {
+        if (resp) {
+          ToastAndroid.show("Course Enrolled successfully!", ToastAndroid.LONG);
+          GetUserEnrolledCourse();
+        }
+      }
+    );
+  };
+
+  const GetUserEnrolledCourse = () => {
+    getUserEnrolledCourse(
+      params.course.id,
+      user.primaryEmailAddress.emailAddress
+    ).then((resp) => {
+      setUserEnrolledCourse(resp.userEnrolledCourses);
+    });
+  };
   return (
-    <View>
-      <Text>CourseDetailScreen</Text>
-    </View>
+    params.course && (
+      <ScrollView style={{ padding: 20 }}>
+        <TouchableOpacity onPress={() => navigate.goBack()}>
+          <Ionicons name="ios-arrow-back-circle" size={40} color="black" />
+        </TouchableOpacity>
+        <DetailSection
+          course={params.course}
+          userEnrolledCourse={userEnrolledCourse}
+          enrollCourse={() => UserEnrollCourse()}
+        />
+      </ScrollView>
+    )
   );
-};
-
-export default CourseDetailScreen;
+}
--- App/services/index.js
+++ App/services/index.js
@@ -43,3 +43,57 @@ export const getCourseList = async (level) => {
   const result = await request(MASTER_URL, query);
   return result;
 };
+
+export const enrollCourse = async (courseId, userEmail) => {
+  const mutationQuery =
+    gql`
+  mutation MyMutation {
+    createUserEnrolledCourse(
+      data: {courseId: "` +
+    courseId +
+    `", 
+      userEmail: "` +
+    userEmail +
+    `", course: {connect: {id: "` +
+    courseId +
+    `"}}}
+    ) {
+      id
+    }
+    publishManyUserEnrolledCoursesConnection(to: PUBLISHED) {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+  `;
+
+  const result = await request(MASTER_URL, mutationQuery);
+  return result;
+};
+
+export const getUserEnrolledCourse = async (courseId, userEmail) => {
+  const query =
+    gql`
+  query GetUserEnrolledCourse {
+    userEnrolledCourses(
+      where: {courseId: "` +
+    courseId +
+    `", 
+        userEmail: "` +
+    userEmail +
+    `"}
+    ) {
+      id
+      courseId
+      completedChapter {
+        chapterId
+      }
+    }
+  }
+  `;
+  const result = await request(MASTER_URL, query);
+  return result;
+};
